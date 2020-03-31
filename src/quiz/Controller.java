252600360e4bc0b82a7af897ea8be48a24d23ced
barrysweeney <barrysweeney172@live.co.uk>
2020-03-31 10:38:35 +0100

    add checkbox question

--- src/quiz/Controller.java
+++ src/quiz/Controller.java
@@ -1,13 +1,17 @@
 package quiz;
 
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+
+import java.util.Arrays;
 
 public class Controller {
+    @FXML
+    public CheckBox q3a1, q3a2, q3a3, q3a4, q3a5;
+
+
 
     /*
      TODO:
@@ -18,7 +22,9 @@ public class Controller {
     */
 
     @FXML
-    ToggleGroup fruit;
+    public ToggleGroup equality;
+    @FXML
+    public ToggleGroup color;
 
     public Button submitButton;
     public Label scoreDisplay;
@@ -27,15 +33,42 @@ public class Controller {
 
     public void submit() {
         // Question 1
-        if (fruit.getSelectedToggle() != null) {
-            RadioButton selectedRadioButton = (RadioButton) fruit.getSelectedToggle();
-            String toogleGroupValue = selectedRadioButton.getText();
-            if (toogleGroupValue.equals("Banana")) {
-                score += 1;
-            }
-        }
+        validateRadioQuestion(equality, "== checks object reference equality (do the references refer to the same object)");
+        // Question 2
+        validateRadioQuestion(color, "Purple");
+        // Question 3
+        validateCheckBoxQuestion(new String[]{"FileNotFoundException", "IOException"}, q3a1,q3a2, q3a3, q3a4, q3a5);
+
         // Updating score display
         scoreDisplay.setText("Score: " + score);
+    }
+
+    private void validateCheckBoxQuestion(String[] correctAnswers, CheckBox... checkBox) {
+        for (CheckBox cb : checkBox) {
+            if (Arrays.asList(correctAnswers).contains(cb.getText())) {
+                cb.setStyle("-fx-color: green");
+                if ((cb.isSelected())) {
+                    score += 1;
+                }
+            } else {
+                cb.setStyle("-fx-color: red");
+            }
+        }
+    }
+
+    private void validateRadioQuestion(ToggleGroup toggleGroup, String correctAnswer) {
+        ObservableList<Toggle> toggles = toggleGroup.getToggles();
+        for (Toggle toggle : toggles) {
+            RadioButton radioButton = (RadioButton) toggle;
+            if (radioButton.getText().equals(correctAnswer)) {
+                radioButton.setStyle("-fx-color:green");
+                if (radioButton.isSelected()) {
+                    score += 1;
+                }
+            } else {
+                radioButton.setStyle("-fx-color: red;");
+            }
+        }
     }
 
 
